255566f91488bf2bde51d4e01d746f6959df035b
Julian Fischer <fischer@avarteq.de>
2010-05-27 11:16:50 +0200

    bugfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   [:div, :ul].each do |method_name|
-    define_method method_name do |options, &block|
-      content_tag method_name, options, &block
+    define_method method_name do |*args, &block|
+      content_tag method_name, args.first, &block
     end
   end
 end
